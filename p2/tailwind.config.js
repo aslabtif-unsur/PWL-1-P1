c8d005f45c6cdcb51ca076b7fdc1ff757412cbc5
aslabtif-unsur <labtif.unsur@gmail.com>
2024-10-10 16:57:28 +0700

    tugas tailwind

---
+++ p2/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      comic: ['Comic Sans MS'],
+      sans: ['sans-serif'],
+      serif: ['serif'],
+      mono: ['consolas'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
